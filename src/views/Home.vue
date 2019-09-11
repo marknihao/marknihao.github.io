cde18ce192deb475d546f2a94fb6defbc2007c23
marknihao <910718622@qq.com>
2019-09-11 16:43:43 +0800

    展示页改为简历

---
+++ src/views/Home.vue
@@ -0,0 +1,233 @@
+<template>
+  <div>
+    <p class="last-modified">更新时间：2019年11月</p>
+    <div class="content">
+      <header class="content-hd">
+            <section class="title">
+                <div class="name">
+                    <h1>杨润飞<small>runfei Yang</small></h1>
+                </div>
+                <div class="job">
+                    <h2>Web前端开发工程师<small> / 深圳</small></h2>
+                </div>
+            </section>
+            <section class="info">
+                <h2>男 / 1990.05</h2>
+                <h3> 6年工作经验</h3>
+            </section>
+            <section class="contact">
+                <ul>
+                    <li><a href="https://github.com/marknihao" target="_blank"><span class="contact-link">https://github.com/marknihao</span><i class="iconfont icon-github"></i></a></li>
+                    <li><a href="mailto:rfeiyang@gmial.cn" target="_blank"><span class="contact-link">rfeiyang@gmial.cn</span><i class="iconfont icon-email"></i></a></li>
+                    <li><a href="tel:18320806285" target="_blank"><span class="contact-link">18320806285</span><i class="iconfont icon-phone"></i></a></li>
+                </ul>
+            </section>
+        </header>
+        <div class="content-bd">
+            <div class="content-left">
+                <section id="experience">
+                    <header class="section-hd">
+                        <span class="section-title-l"></span>
+                        <h2 class="section-title">工作经验</h2>
+                        <span class="section-title-r"></span>
+                    </header>
+                    <div class="section-bd">
+                        <div class="item item-main">
+                            <header class="item-hd">
+                                <span class="item-time">2019.03 ~ 至今</span>
+                                <a class="btn item-more" href="https://www.bj-ihealthcare.com/ward/index.jhtml" target="_blank" title="工作经历">工作</a>
+                                <h3 class="item-name">博钧科技</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>开发部</strong>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <header class="item-hd">
+                                <h3 class="item-name"><i class="iconfont icon-dot"></i> 公司官网 速搭 · 普通web网页</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>研发负责人</strong>
+                                    主要使用 前端主要使用jquery flex,后台系统使用jeeCMS快速搭建
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <header class="item-hd">
+                                <h3 class="item-name"><i class="iconfont icon-dot"></i> 公共卫生管理系统.居民健康档案</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>项目负责人</strong>
+                                    负责团队前端公共基础的建设<em>vue</em>应用于快速搭建前端项目
+                                </p>
+                                <p class="section-content">
+                                    基于<em>element UI</em>开发的样式UI组件库，制定组件规范，保证高复用性，负责组件库整体的建设及其大部分组件的开发工作。
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="section-bd">
+                        <div class="item item-main">
+                            <header class="item-hd">
+                                <span class="item-time">2015.06 ~ 2018.06</span>
+                                <a class="btn item-more" href="http://www.sztaiji.com/index.html" target="_blank" title="工作经历">工作</a>
+                                <h3 class="item-name">深圳太极软件股份有限公司</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>设计部</strong>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <header class="item-hd">
+                                <h3 class="item-name"><i class="iconfont icon-dot"></i> 贵阳市政府数据开放平台 · 普通web网页</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>前端负责人</strong>
+                                    与交互设计师及视觉设计师协作，根据需求完成web页面制作 ;解决不同浏览器，及不同版本的兼容性问题;负责前端的交互动画，以及后台接口的对接；
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <header class="item-hd">
+                                <h3 class="item-name"><i class="iconfont icon-dot"></i> 佛山大屏.数据统计</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>项目负责人</strong>
+                                    政府大屏分辨率7680px*2400px，使用Echarts框架制作数据表，根据客户要求制作动画交互效果，使用iscroll 完成终端机触摸屏的交互效果;
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section-bd">
+                        <div class="item item-main">
+                            <header class="item-hd">
+                                <span class="item-time">2014.05 ~ 2015.06</span>
+                                <a class="btn item-more" href="http://www.isoftstone.com/" target="_blank" title="工作经历">工作</a>
+                                <h3 class="item-name">深圳软通动力科技有限公司</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>开发部</strong>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <header class="item-hd">
+                                <h3 class="item-name"><i class="iconfont icon-dot"></i> 前海保险 · 金融web</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>前端负责人</strong>
+                                    和产品经理沟通并确定产品开发需求,根据产品需求，负责前端代码编写,解决不同浏览器以及不同版本的兼容性问题,协助后台完成部分功能
+                                </p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <header class="item-hd">
+                                <h3 class="item-name"><i class="iconfont icon-dot"></i> 深圳中小企业公共服务平台</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    <strong>项目负责人</strong>
+                                    app及电商网站制作
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="section-bd">
+                        <div class="item item-main">
+                            <header class="item-hd">
+                                <span class="item-time">2013.12 ~ 2014.05</span>
+                                <a class="btn item-more" href="#" target="_blank" title="工作经历">工作</a>
+                                <h3 class="item-name">同进网络科技有限公司</h3>
+                            </header>
+                            <div class="item-bd">
+                                <p class="section-content">
+                                    企业网站制作
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="content-right">
+                <section id="skill">
+                    <header class="section-hd">
+                        <span class="section-title-l"></span>
+                        <h2 class="section-title">专业能力</h2>
+                        <span class="section-title-r"></span>
+                    </header>
+                    <div class="section-bd">
+                        <ul class="section-list">
+
+                           <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                               掌握Web前端开发基本技能，熟悉<strong>W3C标准</strong>、<strong>HTML</strong>、<strong>CSS</strong>、<strong>UI重构</strong>、<strong>页面布局</strong>、<strong>响应式布局</strong>等，领悟<strong>视觉</strong>与<strong>设计</strong>，重视<strong>页面交互</strong>与<strong>用户体验</strong>。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                对<strong>JavaScript</strong>、各类<strong>UI组件库</strong>、<strong>JS类库</strong>、<strong>模板引擎</strong>、<strong>MV*框架</strong>、<strong>工程化工具</strong>等有着较熟练的实践和较深刻的感悟。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>了解php node 后台语言 了解mongdb 数据库，运用node 做过一些小例子实现数据与结构的分离。可以与后台人员进行很好的对接。</p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>以<em>Vue</em><em>less</em><em>sass</em><em>Webpack</em><em>微信小程序</em>等为常用技术栈，同时也了解主流的<em>Node.js</em><em>React</em><em>Angular</em><em>gulp</em>等技术。</p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>熟悉SEO 网站优化，对用户体验、交互操作流程也有一定的了解。</p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>熟悉各种常用的工具：Hbuider ,WebStorm、sublime、vscode、myEclipse、photoshop、AI、Svn、 bash、git等</p></li>
+
+                            <!--
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                对<strong>JavaScript</strong>、各类<strong>UI组件库</strong>、<strong>JS类库</strong>、<strong>模板引擎</strong>、<strong>MV*框架</strong>、<strong>工程化工具</strong>等有着较熟练的实践和较深刻的感悟。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                对<strong>Node.js</strong>、<strong>模块化规范</strong>、<strong>CSS预处理器</strong>、<strong>数据可视化</strong>、<strong>设计模式</strong>、<strong>性能优化</strong>、<strong>前端安全</strong>等也有一定的应用与思考。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                熟练使用<strong>ES6</strong>语法编程，追求代码<strong>高质量</strong><strong>可维护性</strong>。以<em>Vue</em><em>less</em><em>Webpack</em><em>微信小程序</em><em>Node.js</em>等为常用技术栈，同时也了解主流的<em>React</em><em>gulp</em>等技术。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                熟练使用<strong>Git</strong>进行版本控制和代码托管、<strong>Markdown</strong>进行文档编写，了解项目常规开发流程、<strong>开发调试</strong>技巧、<strong>发布部署</strong>步骤，掌握<strong>类Unix</strong>服务器基本运维能力。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                时刻保持<strong>好奇心</strong>，具有一定的<strong>创新创造</strong>精神，持续关注业内前沿技术。工作上<strong>积极执行</strong><strong>有责任心</strong>，具有良好的<strong>沟通合作</strong>能力、<strong>分析解决问题</strong>能力以及较强的<strong>学习能力</strong>。
+                            </p></li> -->
+                        </ul>
+                    </div>
+                </section>
+                <section id="personal">
+                    <header class="section-hd">
+                        <span class="section-title-l"></span>
+                        <h2 class="section-title">自评</h2>
+                        <span class="section-title-r"></span>
+                    </header>
+                    <div class="section-bd">
+                         <ul class="section-list">
+
+                           <li><p class="section-content"><i class="iconfont icon-dot"></i>
+                                时刻保持<strong>好奇心</strong>，具有一定的<strong>创新创造</strong>精神，持续关注业内前沿技术。工作上<strong>积极执行</strong><strong>有责任心</strong>，具有良好的<strong>沟通合作</strong>能力、<strong>分析解决问题</strong>能力以及较强的<strong>学习能力</strong>。
+                            </p></li>
+                            <li><p class="section-content"><i class="iconfont icon-dot"></i>有很强的时间观念，很好的安排时间，可以保障每一天的工作游刃有余。同时我不会吧生活中的情绪带到工作中，做到工作与生活更好的结合。</p></li>
+
+                         </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import '@/assets/style/index.less'
+export default {
+  name: 'home'
+
+}
+</script>
